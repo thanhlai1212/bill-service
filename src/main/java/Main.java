90e82bb2fc957562cbb8bdc560250a670c539851
thanhlai <thanhpo1212@gmail.com>
2023-10-07 21:20:12 +0700

    build jar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,9 @@ import modules.schedule.ScheduleService;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
   public static final AccountService accountService = new AccountService();
@@ -17,27 +19,32 @@ public class Main {
   public static final PaymentService paymentService = new PaymentService();
   public static final ScheduleService scheduleService = new ScheduleService();
 
-  public static void main(String[] args) {
+  public static void main(String[] arguments) {
+    while (true) {
+      Scanner scan=new Scanner(System.in);
+      List<String> args = List.of(scan.nextLine().split(" "));
 
-      if (args[0].equals("CASH_IN")) {
-        cashIn(Long.valueOf(args[1]));
-      } else if (args[0].equals("LIST_BILL")) {
+      if (args.get(0).equals("CASH_IN")) {
+        cashIn(Long.valueOf(args.get(1)));
+      } else if (args.get(0).equals("LIST_BILL")) {
         listBill(billService.getBills());
-      } else if (args[0].equals("PAY")) {
+      } else if (args.get(0).equals("PAY")) {
         pay(args);
-      } else if (args[0].equals("DUE_DATE")) {
+      } else if (args.get(0).equals("DUE_DATE")) {
         listBill(billService.getBillsByDueDate());
-      } else if (args[0].equals("SCHEDULE")) {
+      } else if (args.get(0).equals("SCHEDULE")) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate date = LocalDate.parse(args[2], formatter);
-        schedule(Long.valueOf(args[1]), date);
-      } else if (args[0].equals("LIST_PAYMENT")) {
+        LocalDate date = LocalDate.parse(args.get(2), formatter);
+        schedule(Long.valueOf(args.get(1)), date);
+      } else if (args.get(0).equals("LIST_PAYMENT")) {
         listPayment();
-      } else if (args[0].equals("SEARCH_BILL_BY_PROVIDER")) {
-        searchBillByProvider(BillProvider.valueOf(args[1]));
-      } else if (args[0].equals("EXIT")) {
+      } else if (args.get(0).equals("SEARCH_BILL_BY_PROVIDER")) {
+        searchBillByProvider(BillProvider.valueOf(args.get(1)));
+      } else if (args.get(0).equals("EXIT")) {
         System.out.println("Good bye!");
+        return;
       }
+    }
   }
 
   public static void cashIn(Long amount) {
@@ -54,12 +61,12 @@ public class Main {
     }
   }
 
-  public static void pay(String[] args) {
+  public static void pay(List<String> args) {
     Account account = accountService.getAccount(1L);
     Long totalBillAmount = 0L;
 
-    for (int i = 1; i < args.length; i++) {
-      Long id = Long.valueOf(args[i]);
+    for (int i = 1; i < args.size(); i++) {
+      Long id = Long.valueOf(args.get(i));
       Bill bill = billService.getBill(id);
       if (bill == null) {
         System.out.println("Sorry! Not found a bill with such id " + id);
@@ -72,8 +79,8 @@ public class Main {
     if (account.getBalance() < totalBillAmount) {
       System.out.println("Sorry! Not enough fund to proceed with payment.");
     } else {
-      for (int i = 1; i <= args.length; i++) {
-        Bill bill = billService.getBill(Long.valueOf(args[i]));
+      for (int i = 1; i < args.size(); i++) {
+        Bill bill = billService.getBill(Long.valueOf(args.get(i)));
         account.setBalance(account.getBalance() - bill.getAmount());
         paymentService.addPayment(bill.getAmount(), LocalDate.now(), PaymentState.PROCESSED, bill.getId());
         System.out.println("Payment has been completed for Bill with id " + bill.getId());
